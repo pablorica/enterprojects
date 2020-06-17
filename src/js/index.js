2e7d7947b4ed5b8f31b58ca0719035c1a344978e
Pablo Rica <pabloricaaldeguer@gmail.com>
2020-06-17 17:31:02 +0200

    Feedback 1 amends

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,57 @@ import 'bootstrap';
 
 import '../scss/index.scss';
 
-$('#alert').click(() => {
-  alert('jQuery works!');
-});
+import '../slick/slick.min.js';
 
-// Your jQuery code
+(function($) {
+  /*'use strict';*/
+
+  var CDG = {
+    //OnReady functions
+
+    addMSGonReady: function() {
+      console.log('Codigo Ready');
+    },
+
+    slickslider: function() {
+      if ($('.slick-carousel').length) {
+        var $carousel = $('.slick-carousel');
+        var autoplay = $carousel.data('autoplay'),
+          dots = $carousel.data('dots'),
+          arrows = $carousel.data('arrows');
+        $carousel.slick({
+          lazyLoad: 'ondemand',
+          autoplay: autoplay,
+          dots: dots,
+          arrows: arrows,
+          prevArrow: '<button class="slick-prev slick-arrow">Prev</button>',
+          nextArrow: '<button class="slick-next slick-arrow">Next</button>',
+        });
+      }
+    },
+
+    onreadyFunctions: function() {
+      //CDG.addMSGonReady();
+
+      CDG.slickslider();
+    },
+
+    //OnLoad functions
+
+    addMSGonLoad: function() {
+      console.log('Codigo Loaded');
+    },
+
+    onloadFunctions: function() {
+      //CDG.addMSGonLoad();
+    },
+  };
+
+  $(window).ready(function() {
+    CDG.onreadyFunctions();
+  });
+
+  $(window).on('load', function() {
+    CDG.onloadFunctions();
+  });
+})(jQuery);
